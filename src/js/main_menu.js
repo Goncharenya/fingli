eed711cae8198e6ab186a144fdd33235a9faf129
Roman Goncharenya <goncharenya@gmail.com>
2022-02-11 22:21:02 +0300

    js menu close

--- src/js/main_menu.js
+++ src/js/main_menu.js
@@ -8,7 +8,23 @@ const body = document.querySelector('body'),
 // lineBox.classList.remove("menu__hidden");
 
 lineBox.addEventListener("click", function () {
-
     headerMenu.classList.toggle("_active");
    // lineBox.classList.toggle("menu__close");
+});
+
+
+document.addEventListener("click", function (e) {
+    // кликнули не на кнопку открытие меню
+    if (e.originalTarget !== lineBox) {
+        // открыто ли меню
+        if (headerMenu !== null
+            && headerMenu.classList.contains('_active')
+        ) {
+            // кликнули не на меню
+            if (e.originalTarget.closest('nav.header__menu') === null) {
+                // значит его надо закрыть
+                headerMenu.classList.remove('_active')
+            }
+        }
+    }
 });
