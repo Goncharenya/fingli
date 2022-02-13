8537e0f285fbfe6fa3fcfc1ea2cb39995e410960
Goncharenya <kotmay@mail.ru>
2022-02-13 19:07:17 +0300

    popup

---
+++ src/js/popup_start-project.js
@@ -0,0 +1,30 @@
+const popupStartProject = document.querySelector('.popup'),
+    btnStartProject = document.querySelector('.btn__start-project'),
+    popupClose = document.querySelector('.popup__close');
+
+btnStartProject.addEventListener("click", function () {
+    popupStartProject.classList.add('popup-open');
+
+
+});
+
+popupClose.addEventListener('click', function () {
+    popupStartProject.classList.remove('popup-open');
+})
+
+
+document.addEventListener("click", function (e) {
+    // кликнули не на кнопку открытие меню
+    if (e.target !== btnStartProject) {
+        // открыто ли меню
+        if (popupStartProject !== null
+            && popupStartProject.classList.contains('popup-open')
+        ) {
+            // кликнули не на меню
+            if (e.target.closest('nav.header__menu') === null) {
+                // значит его надо закрыть
+                popupStartProject.classList.remove('popup-open')
+            }
+        }
+    }
+});
