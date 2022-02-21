0e5c3c812c0bf0f7302a4b9a9a3f245074c8a246
Goncharenya <kotmay@mail.ru>
2022-02-21 18:23:00 +0300

    21.02.22

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,3 @@
-// const gulp = require('gulp');
-// const plumber = require("gulp-plumber");
-// const sourcemap = require("gulp-sourcemaps");
-// const postcss = require("gulp-postcss");
-// const autoprefixer = require("gulp-autoprefixer");
-// const csso = require('gulp-postcss');
-// const rename = require('gulp-rename');
-// const browserSync = require('browser-sync');
-// const sass = require('gulp-sass')(require('sass'));
-//
-// gulp.task('server', function () {
-//     browserSync.init({
-//         server: {
-//             baseDir: 'src'
-//         }
-//     });
-//     gulp.watch('src/*.html').on('change', browserSync.reload);
-// });
-//
-// gulp.task('style', function () {
-//     return gulp.src("src/scss/style.scss")
-//         .pipe(sass())
-//         .pipe(plumber())
-//         .pipe(rename("style.min.css"))
-//         .pipe(sourcemap.write("."))
-//         .pipe(gulp.dest("src/css"))
-//         .pipe(browserSync.stream());
-// })
-//
-// gulp.task('watch', function () {
-//     gulp.watch('src/scss/*.+scss', gulp.parallel('style'));
-//
-// })
-//
-// gulp.task('default', gulp.parallel('watch', 'server', 'style'))
-
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
@@ -110,7 +74,7 @@ const watcher = () => {
 const buildJs = () => {
     return gulp.src('src/js/*.js')
         .pipe(jsmin())
-        .pipe(rename('scripts.min.js'))
+        // .pipe(rename('scripts.min.js'))
         .pipe(gulp.dest('build/js'));
     // .pipe(sync.stream());
 }
@@ -156,9 +120,10 @@ const copyOther = (done) => {
         "src/fonts/*.{woff,woff2}",
         "src/*.ico",
         "src/img/*.svg",
+        "src/carousel/*.*",
         "!src/img/icons/*.svg"
     ], {
-        base: "source"
+        base: "src"
     })
         .pipe(gulp.dest('build'));
     done();
@@ -172,8 +137,8 @@ const build = gulp.series(
         styles,
         buildHtml,
         buildJs,
-        createSprite,
-        createWebp
+        // createSprite,
+        // createWebp
     ),
 );
 
@@ -191,8 +156,8 @@ exports.default = gulp.series(
         styles,
         buildHtml,
         buildJs,
-        createSprite,
-        createWebp
+        // createSprite,
+        // createWebp
     ),
     gulp.series(
         server,
